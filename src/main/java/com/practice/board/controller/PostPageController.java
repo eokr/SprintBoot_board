09670abda22cdc56ae5f4612379b0339a457ac8f
이준수 <lqsdf5647@naver.com>
2023-02-20 16:39:19 +0900

    포스트 작성 시 회원정보의 닉네임 정보가 글쓴이로 나오도록 수정

--- src/main/java/com/practice/board/controller/PostPageController.java
+++ src/main/java/com/practice/board/controller/PostPageController.java
@@ -3,11 +3,14 @@ package com.practice.board.controller;
 import com.practice.board.dto.PaginationDto;
 import com.practice.board.dto.PostRequestDto;
 import com.practice.board.dto.PostResponseDto;
+import com.practice.board.entity.User;
 import com.practice.board.service.PostServiceImpl;
+import com.practice.board.service.UserDetail;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -42,20 +45,28 @@ public class PostPageController {
     public String openPostDetail(@RequestParam("idPost") Long idPost,
                                  HttpServletRequest request,
                                  HttpServletResponse response,
+                                 @AuthenticationPrincipal UserDetail userDetail,
                                  Model model) {
         // 조회수 증가(쿠키값을 보고 없으면 증가)
         postServiceImpl.updateViewCount(idPost, request, response);
         // idPost 에 맞는 페이지 가져오기
         PostResponseDto postResponseDto = postServiceImpl.findById(idPost);
         model.addAttribute("postResponseDto", postResponseDto);
+
+        User user = userDetail.getUser();
+        model.addAttribute("userNickName", user.getUserNickName());
         return "postDetail";
     }
 
     /**
      * 포스트 작성 페이지로 이동
+     * @AuthenticationPrincipal 어노테이션과 UserDetails 를 구현한
+     * UserDetail 을 객체로 로그인한 사용자 정보를 가져올 수 있다.
      */
     @GetMapping("/write")
-    public String writePost() {
+    public String writePost(@AuthenticationPrincipal UserDetail userDetail, Model model) {
+        User user = userDetail.getUser();
+        model.addAttribute("userNickName", user.getUserNickName());
         return "writePost";
     }
 
@@ -66,5 +77,15 @@ public class PostPageController {
     public String submitWritePost(PostRequestDto postRequestDto){
         postServiceImpl.save(postRequestDto);
         return "redirect:/post/list?page=1";
+    }
+
+    /**
+     * 포스트 수정 페이지로 이동
+     * 작성 필요
+     */
+    @GetMapping("/edit")
+    public String editPost() {
+
+        return "editPost";
     }
 }
